f814285e26a859568578e7a2a6b4d42d91dd70a7
sibaprasad <sibap865@gmail.com>
2023-10-16 15:51:44 +0530

    requirements file setup file modified

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author= 'sibaprasad',
+    author_email= 'sibap865@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
